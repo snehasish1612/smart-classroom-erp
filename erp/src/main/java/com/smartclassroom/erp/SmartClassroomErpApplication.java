32f088e9cabfd7610898e69b714afe8707ef67e4
Snehasish Mukherjee <snehasish1612@gmail.com>
2026-02-22 23:41:47 +0530

    Initial Spring Boot setup with DB config

---
+++ erp/src/main/java/com/smartclassroom/erp/SmartClassroomErpApplication.java
@@ -0,0 +1,13 @@
+package com.smartclassroom.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartClassroomErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartClassroomErpApplication.class, args);
+	}
+
+}
